3b457d36f0eefb851fec85d7c331ef56cc653583
Fernando Tavares <fernandotavares397@gmail.com>
2022-10-21 00:13:37 -0300

    Aplicado a operação de divisão

--- App.js
+++ App.js
@@ -32,7 +32,7 @@ export default function App() {
                 resultNumber = resultNumber + parseInt(value);
                 break
               case '/': 
-                resultNumber = resultNumber - parseInt(value);
+                resultNumber = resultNumber / parseInt(value);
                 break
             }
           } else {
